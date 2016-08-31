08bccb31599c2a5c2edf1c6c83d10ebdf1ebbd5d
splatsid <siddu_787@hotmail.com>
2016-09-01 11:49:36 +1200

    facebook sharing, working weather generator

---
+++ weatherApp/TypeScriptHTMLApp1/TypeScriptHTMLApp1/app.ts
@@ -0,0 +1,16 @@
+﻿var $;
+var jQuery;
+$(document).ready(function () {
+    jQuery.simpleWeather({
+        location: 'Auckland, NZ',
+        woeid: '',
+        unit: '&deg' + 'C',
+        success: function (weather) {
+            document.getElementById("location").innerHTML = weather.city;
+            document.getElementById("temperature").innerText = weather.temp + weather.units.temp;
+        },
+        error: function (error) {
+            jQuery("location").text(location);
+        }
+    });
+});
---
+++ weatherLogic.js
@@ -0,0 +1,21 @@
+var $;
+var position;
+var timezoneJs;
+position = 'Auckland, NZ';
+var button = document.getElementById("btn");
+button.onclick = function () {
+    $(document).ready(function () {
+        $.simpleWeather({
+            location: position,
+            woeid: '',
+            unit: 'C',
+            success: function (weather) {
+                document.getElementById("location").innerHTML = weather.city;
+                document.getElementById("temperature").innerText = weather.temp + weather.units.temp;
+            },
+            error: function (error) {
+                document.getElementById("location").innerHTML = "Error";
+            }
+        });
+    });
+};
