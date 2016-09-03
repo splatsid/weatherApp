916853006519fef4455d885b71e7f7098ba8cac9
splatsid <siddu_787@hotmail.com>
2016-09-03 18:27:33 +1200

    geolocation, new file
    new file uses bootstrap and html5

--- weatherLogic.js
+++ weatherLogic.js
@@ -1,20 +1,31 @@
 var $;
 var position;
 var timezoneJs;
-position = 'Auckland, NZ';
+var loc;
+if ("geolocation in navigator") {
+    position = navigator.geolocation.getCurrentPosition(function (position) {
+        loc = position.coords.latitude + ',' + position.coords.longitude;
+    });
+}
+else {
+    loc = 'Auckland, NZ';
+}
+if (position == null) {
+    loc = 'Auckland, NZ';
+}
 var button = document.getElementById("btn");
 button.onclick = function () {
     $(document).ready(function () {
         $.simpleWeather({
-            location: position,
+            location: loc,
             woeid: '',
             unit: 'C',
             success: function (weather) {
                 document.getElementById("location").innerHTML = weather.city;
-                document.getElementById("temperature").innerText = weather.temp + weather.units.temp;
+                document.getElementById("temperature").innerHTML = weather.temp + "°" + weather.units.temp;
             },
             error: function (error) {
-                document.getElementById("location").innerHTML = "Error";
+                document.getElementById("location").innerHTML = "WTF";
             }
         });
     });
